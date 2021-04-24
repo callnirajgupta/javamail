f3b711fc2e93dae9498104329ca13a12dff3740a
callnirajgupta <callnirajgupta@gmail.com>
2021-04-24 16:40:08 +0530

    add project

---
+++ src/main/java/com/roboticautomation/JavaMail/App.java
@@ -0,0 +1,13 @@
+package com.roboticautomation.JavaMail;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
